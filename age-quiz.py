2dbfd8c998044a70467861497e281b76428bbefc
usernamejayden <jaybalaquidan@hotmail.com>
2024-03-27 21:37:36 +0000

    added

--- age-quiz.py
+++ age-quiz.py
@@ -1,6 +1,7 @@
-age = int(input("What is your age? "))
-if age > 100:
-    print("Sorry, you're dead.")
+# This code will ask the user for their age
+age = int(input("What is your age? ")) # allows the user to input 
+if age > 100: # looks for an age less than 100
+    print("Sorry, you're dead.") # prints out a response based on the user input if it was >100
 elif age > 65:
     print("Enjoy your reitrement!")
 elif age > 40:
